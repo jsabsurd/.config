74c1dfa80d5132ab704937756ca0879f557b2616
Johnny Stauch <joh.stauch@proton.me>
2025-06-05 23:54:45 +0200

    removed .zshrc, added fish.config aliases

--- fish/config.fish
+++ fish/config.fish
@@ -5,4 +5,13 @@ end
 alias ff="fastfetch"
 alias ls="exa --icons --group-directories-first"
 alias ll="exa -lah --git"
+alias vpncn="sudo openvpn --config /etc/openvpn/mullvad_se_all.conf"
+alias rat="sudo pacman -S"
+alias upf="sudo pacman -Syyuu"
+alias uyf="yay -Syyuu"
+alias crb1="paccache -rk1"
+alias cru="paccache -ruk0"
+alias aggrrc="sudo pacman -Scc"
+alias deutsch="setxkbmap de"
+alias maky="sudo make clean install"
 starship init fish | source
