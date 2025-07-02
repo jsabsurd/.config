f3345fa56d683f5e08a7bae994bbb034fa89e901
Johnny Stauch <joh.stauch@proton.me>
2025-07-02 14:46:15 +0200

    backup

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
+set fish_greeting
 alias ff="fastfetch"
 alias ls="exa --icons --group-directories-first"
 alias ll="exa -lah --git"
@@ -14,4 +14,5 @@ alias cru="paccache -ruk0"
 alias aggrrc="sudo pacman -Scc"
 alias deutsch="setxkbmap de"
 alias maky="sudo make clean install"
+alias pinas="sudo mount -t cifs -o username=bonell //192.168.2.201/pinas /mnt/pinas/"
 starship init fish | source
